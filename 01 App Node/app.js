3ac4443fd5a2c218b528e1f7788404d8f981f0f5
mrp4sten <mauricio.pasten.martinez@gmail.com>
2024-04-15 18:20:14 -0600

    Undertanding communication and networking of Docker

--- 01 App Node/app.js
+++ 01 App Node/app.js
@@ -1,9 +1,10 @@
 const { createServer } = require('node:http');
 
-const hostname = '127.0.0.1';
+const hostname = '172.17.0.2';
 const port = 3000;
 
 const server = createServer((req, res) => {
+  console.log('Request From :>> ', req.socket.remoteAddress);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
